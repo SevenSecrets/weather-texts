0ebe12df527c87ebd89f66a565e2841142389636
SevenSecrets <freddieshughes@googlemail.com>
2020-05-20 19:04:36 +0100

    adding phone numbers as env variables

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -1,11 +1,13 @@
 require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load
 
 account_sid = ENV['TWILIO_ACCOUNT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 client = Twilio::REST::Client.new(account_sid, auth_token)
 
-from = '+???' # Your Twilio number
-to = '+???' # Your mobile phone number
+from = ENV['TWILIO_FROM_NO']
+to = ENV['YOUR_PHONE_NO']
 
 client.messages.create(
   from: from,
