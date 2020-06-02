0d3bcaeb4436b29504a55bff5d55ad9893bb1d48
SevenSecrets <freddieshughes@googlemail.com>
2020-06-02 16:32:54 +0100

    heroku test build

--- lib/app.rb
+++ lib/app.rb
@@ -13,6 +13,6 @@ post '/new-message' do
   message = JSON.parse(request.body.read)
   controller = Controller.new
   controller.get_data
-  controller.compose_message
+  p controller.compose_message
   controller.send_message(message["message"]["chat"]["id"])
 end
