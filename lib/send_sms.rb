43b5c2eab8f8d0663d215a1486da4c0b93ae7258
SevenSecrets <freddieshughes@googlemail.com>
2020-06-02 16:29:34 +0100

    heroku test build

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -4,18 +4,17 @@ Dotenv.load
 
 class SendMessage
   def initialize
-    # @account_sid = ENV['TWILIO_ACCOUNT_SID']
-    # @auth_token = ENV['TWILIO_AUTH_TOKEN']
-    # @client = Twilio::REST::Client.new(@account_sid, @auth_token)
-    # @from = ENV['TWILIO_FROM_NO']
-    # @to = ENV['YOUR_PHONE_NO']
     @telegramAPI = ENV['TELEGRAM_API_KEY']
     @message = ""
   end
 
   def send_message(message, chat_id = "none")
     header = { chat_id: chat_id, text: message }
-    RestClient.post("https://api.telegram.org/bot" + @telegramAPI + "/sendMessage", {}, header)
+    begin
+      RestClient.post("https://api.telegram.org/bot" + @telegramAPI + "/sendMessage", {}, header)
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end 
     p "message sent"
-  end 
+  end
 end
